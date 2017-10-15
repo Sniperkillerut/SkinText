9148ed1bbe6fb625050583bc2a9a17e06cda4933
Sniperkillerut <christian.a.gil@gmail.com>
2017-10-14 20:38:47 -0500

    added quick show before hiding skintext window

--- SkinText/ShowMessageCommand.cs
+++ SkinText/ShowMessageCommand.cs
@@ -16,6 +16,7 @@ namespace SkinText {
                         item.Activate();
                     }
                     else {
+                        item.Show();
                         item.Hide();
                     }
                 }
