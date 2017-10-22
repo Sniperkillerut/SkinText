31e58fe5e354ce35493a5be89200d25dd6bf705b
Sniperkillerut <christian.a.gil@gmail.com>
2017-10-22 18:12:52 -0500

    version bump 0.5.16

--- SkinText/Properties/AssemblyInfo.cs
+++ SkinText/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // mediante el carácter '*', como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.15.*")]
+[assembly: AssemblyVersion("0.5.16.*")]
 [assembly: NeutralResourcesLanguage("en")]
